d7a79b563b37d33283c7f1f3098d182807d00598
JUGGRU SER <89448391+JUGGRU-SER-12@users.noreply.github.com>
2021-08-26 12:07:55 +0530

    Update auto.js

--- plugins/auto.js
+++ plugins/auto.js
@@ -12,22 +12,22 @@ r_text[2] = "JUGGRU says i love you 😘";
 r_text[3] = "JUGGRUSERdeyude yudhangal Company Kanan Povunnatheyuloo";
 r_text[4] = "hi, how are you";
 r_text[5] = "ennikk korch rest theroo...🥵🥵";
-r_text[6] = "JUGGRUSER - eva - simsim \ningane ethra ethra paerugal aan alle enikk..😂🤣";
+r_text[6] = "JUGGRUSER - eva - blind juggru \ningane ethra ethra paerugal aan alle enikk..😂🤣";
 r_text[7] = "താനാരാണെന്ന് തനിക്ക് അറിയാന്‍ മേലങ്കില്‍താന്‍ എന്നോട്‌ ചോദിക്ക്.. താനാരണെന്ന്...തനിക്ക് ഞാന്‍ പറഞ്ഞു തരാംതാനാരാണെന്ന്...എന്നിട്ട്‌ഞാനാരാണെന്ന് എനിക്കാറിയാമൊന്നുതാനെന്നോട്‌ ചോദിക്ക്..അപ്പോ തനിക്ക് ഞാന്‍ പറഞ്ഞു തരാംതാനാരാണെന്നും ..ഞാനാരാണെന്നും - കുതിരവട്ടം പപ്പു, മണിച്ചിത്രതാഴ് ";
 r_text[8] = "\n enne vilicho...?🧐 \n";
-r_text[9] = "ente per amalser ,ninte per donkey, backi ellarum monkey😼😼";
+r_text[9] = "ente per juggruser ,ninte per donkey, backi ellarum monkey😼😼";
 r_text[10] = "nee vilicha enikk villi kelkathe irrikkaan pattuo";
-r_text[11] = "do you know who is my creator..? its none other than Amal";
+r_text[11] = "do you know who is my creator..? its none other than juggruser";
 r_text[12] = "രണ്ട് ബക്കറ്റ് നിറയെ വെള്ളമുണ്ട്. അതിൽ ഒരു ബക്കറ്റിനു ദ്വാരമുള്ളതാണ്. എന്നാൽ ദ്വാരമുള്ള ബക്കറ്റിൽ നിന്നും വെള്ളം പോകുന്നില്ല. കാരണം എന്താണ്? para ninak vivaram indo nokkatte";
-r_text[13] = ".song adich thangalude fav song name para Amalser ath download aakki theraam";
+r_text[13] = ".song adich thangalude fav song name para juggruser ath download aakki theraam";
 r_text[14] = "ഹിന്ദിക്കാർ പോക്കറ്റിലും മലയാളികൾ അടുപ്പിലും വെക്കുന്ന സാധനം എന്ത്..?  ennallum ath enthaayikkum...🤔🤔";
 r_text[15] = "hi.. ningal ente fan aano..?";
 r_text[16] = "ivide aaro ente per villichallo.. nee aano..?";
 r_text[17] = "\n hey mishter ningalude per entha..?";
-r_text[18] = "enikk njan und Amalser uyir";
+r_text[18] = "enikk njan und juggruser uyir";
 r_text[19] = "\n eda sathyam para nee ente fan alle..\n ";
 r_text[20] = "The coefficient's of linear expansions is the position of Haemoglobin in the atmosphere. But Why? Full Many gem of purests serene lay underneath the unfathomed ocean's unblossomeds ! But why? But why";
-r_text[21] = "Kochi pazhaya kochiyellennariyam… pakshe amalser pazhaya JUGGRUSER thanneya...😎\n\n";
+r_text[21] = "Kochi pazhaya kochiyellennariyam… pakshe juggruser pazhaya JUGGRUSER thanneya...😎\n\n";
 r_text[22] = "Kaanaan oru look illanney ullu… bhayankara budhiya\n\n ninnak allatto enik Amalser";
 r_text[23] = "Ithalla ithinapparam chaadi kadannavananee K.K. JUGGRUSER";
 r_text[24] = "\nVarrierey… Enthado njan inganey aayi poyathu\n";
